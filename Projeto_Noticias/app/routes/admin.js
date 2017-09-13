c50e9aa534e7cd069e1bb7023ea2e882285394d8
Helio Lima <helio.augustolima@gmail.com>
2017-09-12 23:41:25 -0300

    Aulas 45 e 46

--- Projeto_Noticias/app/routes/admin.js
+++ Projeto_Noticias/app/routes/admin.js
@@ -1,6 +1,6 @@
 module.exports = function(app){
     app.get('/formulario_inclusao_noticia', function(req, res){
-        res.render('admin/form_add_noticia');
+        res.render('admin/form_add_noticia', {errosValidacao: {}, noticia: {}});
     });
 
     app.post('/noticias/salvar', function(req, res){
@@ -16,7 +16,7 @@ module.exports = function(app){
         var erros = req.validationErrors();
 
         if(erros){
-            res.render('admin/form_add_noticia', {errosValidacao: erros});
+            res.render('admin/form_add_noticia', {errosValidacao: erros, noticia:noticia});
             return;
         }
 
