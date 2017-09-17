e689ddf8c027f7602df6cc56451f27a799a8496e
Helio Lima <helio.augustolima@gmail.com>
2017-09-17 14:20:16 -0300

    Aula 51

--- Projeto_Noticias/config/server.js
+++ Projeto_Noticias/config/server.js
@@ -7,6 +7,8 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views')
 
+//aponta qual o diretório raiz do conteúdo estátio: css, js....
+app.use(express.static('./app/public'))
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(expressValidator());
 
