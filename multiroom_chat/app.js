e447240e040771cc5a93afef21913ebcdfbd7c93
Helio Lima <helio.augustolima@gmail.com>
2017-09-24 02:12:23 -0300

    Aula 66 - Multirrom_chat: Abrindo conexão via websocket

--- multiroom_chat/app.js
+++ multiroom_chat/app.js
@@ -6,4 +6,13 @@ var server = app.listen(8080, function(){
     console.log('Servidor online');
 });
 
-require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
+
+/* criar a conexão por websocket */
+io.on('connection', function(socket){
+    console.log('usuário conectou');
+
+    socket.on('disconnect', function(){
+        console.log('usuário desconectou');
+    })
+});
